a35fbd0c5d3b2a64f5d8802c91dc7b4f6c6835c6
Stefan Untura <u_stefan@mail.com>
2022-12-05 17:57:50 +0100

    CAR-14: Remove fragments, update sign up page styles and refactor MainActivity
    - Add global color variables
    - Add minHeight of '48dp' for sign up input elements
    - Add SignUp activity class
    - Add SignUp activity to AndroifManifest.xml file
    - Add redirection to sign up page button in 'StartingScreen' activity
    - Refactor 'MainActivity' class name to 'StartingScreen'
    - Remove fragments activities
    - Remove fragments layouts
    - Remove navigation directory

---
+++ CarAuctionApp/app/src/main/java/com/example/carauctionapp/SignUp.java
@@ -0,0 +1,14 @@
+package com.example.carauctionapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SignUp extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_page);
+    }
+}
