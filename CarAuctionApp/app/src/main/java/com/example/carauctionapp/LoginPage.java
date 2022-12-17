ce26de7e843fcd15fb180a17ef1e3bb17841221d
tadas0420 <tadas0420@gmail.com>
2022-12-17 20:40:26 +0100

    update

--- CarAuctionApp/app/src/main/java/com/example/carauctionapp/LoginPage.java
+++ CarAuctionApp/app/src/main/java/com/example/carauctionapp/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.carauctionapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,7 +14,7 @@ import java.util.regex.Pattern;
 public class LoginPage extends Activity {
     private Button logInButton;
 
-    private String  email, password;
+    private String email, password;
     private EditText emailInput, passwordInput;
     private boolean isAllFieldsChecked = false;
 
@@ -27,12 +28,12 @@ public class LoginPage extends Activity {
 
         //EditView inputs
         emailInput = findViewById(R.id.LogInEmail);
-        passwordInput = findViewById(R.id.LogInPassword);
+        passwordInput = findViewById(R.id.loginPassword);
 
-        //Log In button
+        //Sign Up button
         logInButton = findViewById(R.id.login_button);
 
-        //Log in button click event handler
+        //Sign Up button click event handler
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,6 +41,8 @@ public class LoginPage extends Activity {
                     //Temporary data for checks
                     String tempEmail = emailInput.getText().toString();
                     String tempPassword = passwordInput.getText().toString();
+
+                    isAllFieldsChecked = validateAllFields(tempEmail, tempPassword);
 
                     if (isAllFieldsChecked) {
                         LoginPage.this.finish();
@@ -59,7 +62,6 @@ public class LoginPage extends Activity {
             passwordInput.setError("Password length must be at least: " + Utilities.MIN_PASSWORD_LENGTH);
             return false;
         }
-
         if (passwordInput.length() > Utilities.MAX_PASSWORD_LENGTH) {
             passwordInput.setError("Password is too long, max characters: " + Utilities.MAX_PASSWORD_LENGTH);
             return false;
@@ -68,4 +70,46 @@ public class LoginPage extends Activity {
         return true;
     }
 
+    private boolean validateEmailFieldsInput(String email) {
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailInput.setError("Invalid email address!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validatePasswordFieldsInput(String password) {
+        //PASSWORD
+        //Patterns password must contain
+        Pattern upperCase = Pattern.compile("[A-Z]");
+        Pattern lowerCase = Pattern.compile("[a-z]");
+
+        if (!upperCase.matcher(password).find()) {
+            passwordInput.setError("Password must contain upper case characters");
+            return false;
+        }
+        if (!lowerCase.matcher(password).find()) {
+            passwordInput.setError("Password must contain lower case characters");
+            return false;
+        }
+        if (!digit.matcher(password).find()) {
+            passwordInput.setError("Password must contain digits");
+            return false;
+        }
+
+        //Patterns password must not contain
+        if(illegalCharacters.matcher(password).find()) {
+            passwordInput.setError("Illegal characters detected!");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean validateAllFields(String email, String password) {
+        return  validateEmailFieldsInput(email) && validatePasswordFieldsInput(password);
+    }
+
 }
+
