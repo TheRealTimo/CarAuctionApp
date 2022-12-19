ccdfa07fe1f76086fd64e59d86f33eb6b3fa2160
Stefan Untura <u_stefan@mail.com>
2022-12-19 13:12:24 +0100

    CAR-18: Remove api call method from page
    - Remove api call method from page as info will be set from the general car listings page

--- CarAuctionApp/app/src/main/java/com/example/carauctionapp/CarInfo.java
+++ CarAuctionApp/app/src/main/java/com/example/carauctionapp/CarInfo.java
@@ -6,15 +6,6 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-
 public class CarInfo extends Activity {
     //Variables for api request
     private String make, model;
@@ -37,29 +28,7 @@ public class CarInfo extends Activity {
         renderCarInfoDataOnPage();
     }
 
-//    protected void getDataFromApi () {
-//        RequestQueue volleyQueue = Volley.newRequestQueue(CarInfo.this);
-//
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-//                Request.Method.GET,
-//                Utilities.CAR_API_URL,
-//                null,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        powerData.setText();
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        error.printStackTrace();
-//                    }
-//                }
-//        );
-//
-//        volleyQueue.add(jsonObjectRequest);
-//    }
+
     public void setMake(String make) {
         this.make = make;
     }
