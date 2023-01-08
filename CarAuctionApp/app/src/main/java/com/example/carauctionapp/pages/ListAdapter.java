bf7105a8d2946e4e9acf8b1f0c3c9eb245217844
tadas0420 <tadas0420@gmail.com>
2023-01-08 16:29:55 +0100

    Car 16 listings (#22)
    - Implement car listings page
    - Implement passing data from clicked listing page to the info page of the listing

---
+++ CarAuctionApp/app/src/main/java/com/example/carauctionapp/pages/ListAdapter.java
@@ -0,0 +1,47 @@
+package com.example.carauctionapp.pages;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.carauctionapp.R;
+
+import java.util.ArrayList;
+
+public class ListAdapter extends ArrayAdapter<Listing> {
+
+    public ListAdapter(Context context, ArrayList<Listing> listingArrayList){
+        super(context, R.layout.list_item, listingArrayList);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        Listing listing = getItem(position);
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        ImageView listingImage = convertView.findViewById(R.id.listingImage);
+        TextView topBid = convertView.findViewById(R.id.topBid);
+        TextView auctionName = convertView.findViewById(R.id.auctionName);
+        TextView bidAmount = convertView.findViewById(R.id.bidAmount);
+
+        listingImage.setImageResource(listing.imageId);
+        topBid.setText((int) listing.currentBid);
+        auctionName.setText(listing.name);
+        bidAmount.setText(listing.bidAmount);
+
+
+        return convertView;
+    }
+}
---
+++ CarAuctionApp/app/src/main/java/com/example/carauctionapp/pages/Listing.java
@@ -0,0 +1,56 @@
+package com.example.carauctionapp.pages;
+
+public class Listing {
+
+    String name, color;
+    double currentBid;
+    int bidAmount, imageId;
+
+    public Listing(String name, String color, double currentBid, int bidAmount, int imageId) {
+        this.name = name;
+        this.color = color;
+        this.currentBid = currentBid;
+        this.bidAmount = bidAmount;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCurrentBid() {
+        return currentBid;
+    }
+
+    public void setCurrentBid(double currentBid) {
+        this.currentBid = currentBid;
+    }
+
+    public int getBidAmount() {
+        return bidAmount;
+    }
+
+    public void setBidAmount(int bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
